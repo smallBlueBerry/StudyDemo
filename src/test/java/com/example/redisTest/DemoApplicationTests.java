acf4c8bc371b2aebbfcdee6eb6ebbc7243c0f7c6
nanli <nanli@emotibot.com>
2020-01-17 14:34:22 +0800

    update

--- src/test/java/com/example/redisTest/DemoApplicationTests.java
+++ src/test/java/com/example/redisTest/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.redisTest;
 
 import org.junit.Assert;
 import org.junit.Test;
--- src/test/java/com/example/redisTest/RedisTest.java
+++ src/test/java/com/example/redisTest/RedisTest.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.redisTest;
 
 import javax.annotation.Resource;
 
